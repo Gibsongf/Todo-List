abc4d8001e61ed8b863bd664193f2d04bf94089b
Gibsongf <maccgiiibson@gmail.com>
2022-12-24 13:21:35 -0300

    date-mod done

--- src/date-mod.js
+++ src/date-mod.js
@@ -1,47 +1,78 @@
 function date_or_weekday() {
-    const {
-      format,
-      addDays,
-      parseISO,
-      formatDistanceToNow,
-      intlFormat,
-    } = require("date-fns");
-   /* let taskDate = "2022-12-30"; */  
-    function currentDate() {
-      const result = format(new Date(), "yyyy-MM-dd,EEEE");
-      let currentDay = result.split(",")[0];
-      let weekDayName = result.split(",")[1];
-      return { currentDay, weekDayName };
-    }
+  const {
+    format,
+    parseISO,
+    formatDistanceToNowStrict,
+    intlFormat,
+    addDays,
+    addMonths,
+  } = require("date-fns");
   
-    function checkDistance(taskDate) {
-      const formateDate = currentDate();
-      const day_distance = formatDistanceToNow(new Date(taskDate));
-      const days = Number(day_distance.split(" ")[0]) ;
-      /* need fix the exact time that will pass or all the return value will be wrong */
-      console.log(days);
-      /* if (days == 7) {
-        console.log(day_distance, "ddd");
-        return "Next " + formateDate.weekDayName;
-      } */
-      if (days > 7) {
-        let date_number = addDays(parseISO(formateDate.currentDay), days);
-        const fDate = intlFormat(date_number, {
-          year: "numeric",
-          month: "numeric",
-          day: "numeric",
-        });
-        console.log("date-number", fDate);
-        return fDate;
-      }
-      if (days <= 7) {
-        console.log("week day name", formateDate.weekDayName);
-        return formateDate.weekDayName;
-      }
+  function addDays_currentDate(distance, currentDate) {
+    const currentDay = format(new Date(), "yyyy-MM-dd");
+    let intDistance = Number(distance.split(" ")[0]);
+    const distanceName = distance.split(" ")[1];
+    const obj = {
+      days: addDays,
+      day: addDays,
+      months: addMonths,
+      month: addMonths,
+    };
+
+    if (currentDate == currentDay) {
+      console.log(currentDate, currentDate == currentDay, currentDay);
+      return true;
     }
-    return {checkDistance};
+
+    if (distanceName == "hours") {
+      intDistance = 1;
+      let nextWeekDay = "Tomorrow";
+      return { nextWeekDay, intDistance };
+    }
+
+    if (distanceName == "day" || distanceName == "days") {
+      intDistance += 1;
+    }
+
+    const date_number = obj[distanceName](parseISO(currentDay), intDistance);
+    /* console.log(date_number) */
+    const fDate = intlFormat(date_number, {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      weekday: "long",
+    });
+    let nextWeekDay = fDate.split(",")[0];
+    const nextDate = fDate.split(",")[1].trim();
+
+    if (distanceName == "month" || distanceName == "months") {
+      /* console.log(distanceName) */
+      intDistance = 30;
+    }
+    return { nextWeekDay, nextDate, intDistance, currentDay };
   }
-export default date_or_weekday
-/* 
-let test = date_or_weekday('2022-12-30')
-console.log(test.checkDistance()) */
+
+  function checkDistance(taskDate) {
+    const distance = formatDistanceToNowStrict(new Date(taskDate));
+    const formateDate = addDays_currentDate(distance, taskDate);
+    if (formateDate == true) {
+      return "Today";
+    }
+
+    if (formateDate.intDistance == 7) {
+      return "Next " + formateDate.nextWeekDay;
+    }
+    if (formateDate.intDistance > 7) {
+      return formateDate.nextDate;
+    }
+    if (formateDate.intDistance < 7) {
+      console.log("week day name", taskDate);
+      return formateDate.nextWeekDay;
+    }
+  }
+  return { checkDistance };
+}
+export default date_or_weekday;
+
+/* let test = date_or_weekday()
+console.log(test.checkDistance('2022-12-26')) */
